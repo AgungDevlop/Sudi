72d2befea032e4ac5a3bd0b97ac051991f0471fb
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-07 00:51:39 +0800

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -28,7 +28,7 @@ export function PlayVideo() {
   // Array URL untuk pop-under
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://viiukuhe.com/dc/?blockID=427841'
+    'https://wwp.giriucat.com/redirect-zone/5137a848'
   ];
 
   useEffect(() => {
