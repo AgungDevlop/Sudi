1a92ce97fe8e6c753633b37b2dd6b3c00afd6172
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-12 02:18:52 +0500

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -28,7 +28,7 @@ export function PlayVideo() {
   // Array URL untuk pop-under
   const randomUrls = [
     'https://dulyhagglermounting.com/2082665',
-    'https://sorrowfulpsychology.com/HE9TFh'
+    'https://bmadss.com/get/?spot_id=2026313&cat=25&subid=1576043020'
   ];
 
   useEffect(() => {
